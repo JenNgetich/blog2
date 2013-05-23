267a1098b6236838ec89252cedd7a75970424e83
Jennifer Ngetich <jennifer.ngetich@gmail.com>
2013-05-23 12:50:25 -0400

    update

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+  def new
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.build
+  end
+
+  def create
+    @post = Post.find params[:post_id]
+    @comment = @post.comments.build comment_params
+    if @comment.save
+      redirect_to @post, notice: 'Got your comment, Dude!'
+    else
+      render :new   
+    end 
+  end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '48fd0aa7981e4c74a933dc10b7e68f66f24054c80c6b7b931576f3d7fe6698af575c0ed0fdfe34d4f0b35c535acb9b2565d9b321286d0a3766955fe4ae4b5a2a'
